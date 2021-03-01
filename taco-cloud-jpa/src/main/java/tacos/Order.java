96662d50b7155078cd81dec4a75a13857023e14d
skolach <skolach@polynet.lviv.ua>
2021-03-01 15:15:31 +0200

    Added @Column annotation for correct column name conversion.

--- taco-cloud-jpa/src/main/java/tacos/Order.java
+++ taco-cloud-jpa/src/main/java/tacos/Order.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -30,30 +31,38 @@ public class Order implements Serializable {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
+  @Column(name = "createdat")
   private Date createdAt;
 
+  @Column(name = "deliveryname")
   @NotBlank(message="Name is required")
   private String deliveryName;  
 
+  @Column(name = "deliverystreet")
   @NotBlank(message="Street is required")
   private String deliveryStreet;
 
+  @Column(name = "deliverycity")
   @NotBlank(message="City is required")
   private String deliveryCity;
 
+  @Column(name = "deliverystate")
   @NotBlank(message="State is required")
   private String deliveryState;
 
+  @Column(name = "deliveryzip")
   @NotBlank(message="Zip code is required")
   private String deliveryZip;
 
+  @Column(name = "ccnumber")
   //@CreditCardNumber(message="Not a valid credit card number")
   private String ccNumber;
 
-  @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
-            message="Must be formatted MM/YY")
+  @Column(name = "ccexpiration")
+  //@Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$", message="Must be formatted MM/YY")
   private String ccExpiration;
 
+  @Column(name = "cccvv")
   @Digits(integer=3, fraction=0, message="Invalid CVV")
   private String ccCvv;
 
--- taco-cloud-jpa/src/main/java/tacos/Taco.java
+++ taco-cloud-jpa/src/main/java/tacos/Taco.java
@@ -3,6 +3,7 @@ package tacos;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -23,6 +24,7 @@ public class Taco {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
+  @Column(name = "createdat")
   private Date createdAt;
 
   @NotNull
