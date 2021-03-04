9aa3de1badaa71d569efbc63cd3f7197ebb54619
skolach <skolach@polynet.lviv.ua>
2021-03-04 17:25:01 +0200

    Next 4.3.2

--- taco-cloud-jpa/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud-jpa/src/main/java/tacos/security/SecurityConfig.java
@@ -1,33 +1,29 @@
 package tacos.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
-  @Autowired
-  private UserDetailsService userDetailsService;
-
   @Bean
   public PasswordEncoder encoder() {
-    return new StandardPasswordEncoder("53cr3t");
+    return new BCryptPasswordEncoder();
   }
 
   @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+  protected void configure(HttpSecurity http) throws Exception {
 
-    auth
-      .userDetailsService(userDetailsService)
-      .passwordEncoder(encoder());
+    http
+      .authorizeRequests()
+        .antMatchers("/design", "/orders")
+          .access("hasRole('ROLE_USER')")
+        .antMatchers("/", "/**").access("permitAll");
   }
-
 }
